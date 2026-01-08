19973bd1b19d530f4a6ebc1fd5855950b47a0a83
Luca Valsecchi <luca.valsecchi@original.land>
2026-01-08 13:20:23 +0100

    Revert "Revert "feat(n8n): customize""
    This reverts commit 202f876c4f44861bbc2f8dd7132cbd42b4d05f69.

--- packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.vue
+++ packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.vue
@@ -1,35 +1,40 @@
 <script setup lang="ts">
+import type { FrontendSettings } from '@n8n/api-types';
 import { useFavicon } from '@vueuse/core';
 import { computed, onMounted, useCssModule, useTemplateRef } from 'vue';
-
-import LogoIcon from './logo-icon.svg';
-import LogoText from './logo-text.svg';
-
+import LogoCollapsed from './logo-icon.svg';
+import LogoExpanded from './original-full.svg';
+//
 const props = defineProps<
 	(
 		| {
-				size: 'large';
+				location: 'authView';
 		  }
 		| {
-				size: 'small';
+				location: 'sidebar';
 				collapsed: boolean;
 		  }
 	) & {
-		releaseChannel?: 'stable' | 'beta' | 'nightly' | 'dev' | 'rc';
+		releaseChannel: FrontendSettings['releaseChannel'];
 	}
 >();
 
-const { size, releaseChannel } = props;
+const { location, releaseChannel } = props;
 
-const showLogoText = computed(() => {
-	if (size === 'large') return true;
-	return !props.collapsed;
+// const showLogoText = computed(() => {
+// 	if (location === 'authView') return true;
+// 	return !props.collapsed;
+// });
+
+const currentLogo = computed(() => {
+	if (location === 'authView') return LogoExpanded;
+	return 'collapsed' in props && props.collapsed ? LogoCollapsed : LogoExpanded;
 });
 
 const $style = useCssModule();
 const containerClasses = computed(() => {
-	if (size === 'large') {
-		return [$style.logoContainer, $style.large];
+	if (location === 'authView') {
+		return [$style.logoContainer, $style.authView];
 	}
 	return [
 		$style.logoContainer,
@@ -40,9 +45,7 @@ const containerClasses = computed(() => {
 
 const svg = useTemplateRef<{ $el: Element }>('logo');
 onMounted(() => {
-	if (!releaseChannel || releaseChannel === 'stable' || !('createObjectURL' in URL)) {
-		return;
-	}
+	if (releaseChannel === 'stable' || !('createObjectURL' in URL)) return;
 
 	const logoEl = svg.value!.$el;
 
@@ -58,8 +61,8 @@ onMounted(() => {
 
 <template>
 	<div :class="containerClasses" data-test-id="n8n-logo">
-		<LogoIcon ref="logo" :class="$style.logo" />
-		<LogoText v-if="showLogoText" :class="$style.logoText" />
+		<component :is="currentLogo" ref="logo" :class="$style.logo" />
+		<!-- <LogoText v-if="showLogoText" :class="$style.logoText" /> -->
 		<slot />
 	</div>
 </template>
@@ -72,34 +75,37 @@ onMounted(() => {
 }
 
 .logoText {
-	margin-left: var(--spacing--5xs);
+	margin-left: var(--spacing-5xs);
 	path {
-		fill: var(--color--text--shade-1);
+		fill: var(--color-text-dark);
 	}
 }
 
-.large {
+.authView {
 	transform: scale(2);
-	margin-bottom: var(--spacing--xl);
+	margin-bottom: var(--spacing-xl);
 
 	.logo,
 	.logoText {
-		transform: scale(1.3) translateY(-2px);
+		// transform: scale(0.4) translateY(-2px);
+		height: 40px;
+		width: auto;
 	}
 
 	.logoText {
-		margin-left: var(--spacing--xs);
-		margin-right: var(--spacing--3xs);
+		margin-left: var(--spacing-xs);
+		margin-right: var(--spacing-3xs);
 	}
 }
 
 .sidebarExpanded .logo {
-	margin-left: var(--spacing--2xs);
+	margin-left: var(--spacing-2xs);
+	width: 100px;
+	height: auto;
 }
 
 .sidebarCollapsed .logo {
 	width: 40px;
-	height: 30px;
-	padding: 0 var(--spacing--4xs);
+	height: 40px;
 }
 </style>
